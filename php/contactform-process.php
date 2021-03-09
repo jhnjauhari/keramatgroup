d673d285e0853fa2168e9c34bf164d8333f9f5a7
jhnjauhari <johan.jauhari96@gmail.com>
2021-03-09 20:37:37 +0800

    added google maps and contact form

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,8 +25,8 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Aria landing page";
+$EmailTo = "johanmukras@gmail.com";
+$Subject = "New message from Keramat landing page";
 
 // prepare email body text
 $Body = "";
